3c14ec36016538bc92d301156d88856c23fa5b16
sibsway11 <72669185+sibsway11@users.noreply.github.com>
2021-03-08 18:08:45 +0000

    did some edits in script

--- single scrn/Assets/Scripts/PlayerMovement.cs
+++ single scrn/Assets/Scripts/PlayerMovement.cs
@@ -9,21 +9,33 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed, maxSpeed, jumpForce;
     [SerializeField] private Collider2D groundCheck;
     [SerializeField] private LayerMask groundLayers;
+    [SerializeField] private bool cancelJumpEnabled;
 
     private float moveDir;
     private Rigidbody2D myRB;
     private bool canJump;
+    private SpriteRenderer mySprite;
 
     private void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
+        mySprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
+        if (moveDir > 0)
+        {
+            mySprite.flipX = false;
+        }
+
+        if (moveDir < 0)
+        {
+            mySprite.flipX = true;
+        }
         var moveAxis = Vector3.right * moveDir;
 
-        if (-maxSpeed < myRB.velocity.x && myRB.velocity.x < maxSpeed)
+        if (Mathf.Abs(myRB.velocity.x) < maxSpeed)
         {
             myRB.AddForce(moveAxis * moveSpeed, ForceMode2D.Force);
         }
@@ -38,7 +50,6 @@ public class PlayerMovement : MonoBehaviour
         }
 
     }
-
     public void Move(InputAction.CallbackContext context)
     {
         moveDir = context.ReadValue<float>();
