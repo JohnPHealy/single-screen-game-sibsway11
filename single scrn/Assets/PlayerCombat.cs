62b37689f56810b93dbd77b846478dac61cb84e4
sibsway11 <72669185+sibsway11@users.noreply.github.com>
2021-03-12 19:30:31 +0000

    added player combat script

---
+++ single scrn/Assets/PlayerCombat.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    [SerializeField] private Collider2D playerCheck;
+    [SerializeField] private LayerMask playerLayers;
+
+
+    private void Update()
+    {
+        if (playerCheck.IsTouchingLayers(playerLayers))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
