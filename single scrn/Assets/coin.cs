98e1c38deabb8dafa86f96c62259f4a40e8e2062
sibsway11 <72669185+sibsway11@users.noreply.github.com>
2021-03-16 18:01:58 +0000

    added coin script

---
+++ single scrn/Assets/coin.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private int coins = 1;
+    [SerializeField] private Collider2D playerCheck;
+    [SerializeField] private GameManager manager;
+    [SerializeField] private LayerMask playerLayers;
+    private void Update()
+    {
+        if (playerCheck.IsTouchingLayers(playerLayers))
+        {
+            manager.Addscore(coins);
+            Destroy(gameObject);
+        }
+    }
+}
